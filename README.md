ec999181a707706915135c8936292babd339e59a
Teacher Radh <r.kamal@ph.goit.global>
2023-09-12 11:53:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GoIT JavaScript Lessons
+
+## Each branch will contain each lessons in this module. Please, take a look :)
---
+++ js/example-02.js
@@ -0,0 +1,2 @@
+// Example 2 - Arrays and Strings
+// Write a script to calculate the area of a rectangle with sides, whichvalues values are stored in the values variable as a string. Values guaranteed separated by a space.
